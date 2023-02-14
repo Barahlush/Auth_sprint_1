5fd48d60053a5a30664de2f4a62779af0503a315
Полина <PGVasilyeva@sberbank.ru>
2023-02-14 20:18:03 +0300

    добавила стили и поменяла index страницу

--- auth_service/routers.py
+++ auth_service/routers.py
@@ -1,7 +1,9 @@
 from flask import render_template
+from flask_security import auth_required
 from main import app
 
 
 @app.route('/')
+@auth_required()
 def index() -> str:
     return render_template('index.html')
