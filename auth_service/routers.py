206def8dc128455141b966335bc5289926382e74
Полина <PGVasilyeva@sberbank.ru>
2023-02-15 15:08:16 +0300

    поправила индекс html, убрала security

--- auth_service/routers.py
+++ auth_service/routers.py
@@ -1,9 +1,22 @@
 from flask import render_template
-from flask_security import auth_required
+from flask_jwt_extended import get_current_user, jwt_required
 from main import app
 
 
 @app.route('/')
-@auth_required()
+@jwt_required()  # type: ignore
 def index() -> str:
-    return render_template('index.html')
+    welcome_string = 'Welcome!'
+    current_user = get_current_user()
+    contex = {}
+    if current_user:
+        contex.update({'user': current_user})
+        if current_user.name:
+            welcome_string = f'Welcome back, {current_user.name}!'
+            contex.update({'"user_name': current_user.name})
+        else:
+            welcome_string = 'Welcome back!'
+    contex.update({'welcome_string': welcome_string})
+    if current_user.email:
+        contex.update({'user_email': current_user.email})
+    return render_template('index.html', contex=contex)
