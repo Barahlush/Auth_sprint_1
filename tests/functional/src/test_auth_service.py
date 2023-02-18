78827f78627c63ee73921042352d4ba5341c924f
Полина <PGVasilyeva@sberbank.ru>
2023-02-18 11:35:55 +0300

    Написала несколько тестов с фикстурой без БД
    Посмотри, пожалуйста, как поправить ошибку
    
    src.core.models.UserDoesNotExist: <Model: User> instance matching query does not exist:
    SQL: SELECT "t1"."id", "t1"."email", "t1"."password", "t1"."fs_uniquifier", "t1"."active" FROM "user" AS "t1" WHERE ("t1"."email" = %s) LIMIT %s OFFSET %s
    Params: ['test@mail.ru', 1, 0]

--- tests/functional/src/test_auth_service.py
+++ tests/functional/src/test_auth_service.py
@@ -1,2 +1,85 @@
-def test_placeholder() -> None:
-    assert True
+from functional.settings import TestSettings
+
+settings = TestSettings()
+
+
+class BaseData:
+    name = 'test'
+    email = 'test@mail.ru'
+    password = 'test'
+    fs_uniquifier = 'fgkhfdsfgk_fghg_hghgjghjghklj_kjl'
+    active = True
+    signup_data = {
+        'name': name,
+        'email': email,
+        'password': password,
+        'fs_uniquifier': fs_uniquifier,
+        'active': active,
+    }
+    login_data = {
+        'email': email,
+        'password': password,
+        'fs_uniquifier': fs_uniquifier,
+        'active': active,
+    }
+    invalid_login_data = {
+        'email': email,
+        'password': 'invalid',
+        'fs_uniquifier': fs_uniquifier,
+        'active': active,
+    }
+
+
+def test_sign_up(make_post_request):
+    response = make_post_request('/auth/register', data=BaseData.signup_data)
+    assert response.status_code == 201 or 202
+
+
+def test_success_login(make_post_request):
+    response = make_post_request('/auth/login', BaseData.login_data)
+    assert response.status_code == 201
+    data = response.json()
+    assert data[0].get('access_token') is not None
+    assert data[1].get('refresh_token') is not None
+
+
+def test_failed_login(make_post_request):
+    response = make_post_request('/auth/login', BaseData.invalid_login_data)
+    assert response.status_code == 403
+
+
+def test_change_password(make_post_request):
+    response = make_post_request('/auth/login', BaseData.login_data)
+    assert response.status_code == 201
+
+    data = response.json()
+    access_token = data[0]
+    assert access_token is not None
+
+    email = BaseData.email
+    old_password = BaseData.password
+    password = 'change'
+    change_data = {
+        'email': email,
+        'old_password': old_password,
+        'new_password': password,
+    }
+    change = make_post_request(
+        '/auth/change_password', data=change_data, headers=access_token
+    )
+    assert change.status_code == 200
+
+    response = make_post_request(
+        '/auth/login', {'email': email, 'password': password}
+    )
+    assert response.status_code == 201
+
+    return_old_password = {
+        'email': email,
+        'old_password': password,
+        'new_password': old_password,
+    }
+    change = make_post_request(
+        '/auth/change_password', data=return_old_password, headers=access_token
+    )
+    assert change.status_code == 200
