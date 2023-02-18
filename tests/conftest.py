76267fab2c02d688da2e3ba4c6f763706a020dff
Полина <PGVasilyeva@sberbank.ru>
2023-02-18 20:56:37 +0300

    доделала тесты, поменяла makefile и компоуз для запуска тестов
    при run и WTF_CSRF_ENABLED=False работает только напрямую. через os.get не работает

--- tests/conftest.py
+++ tests/conftest.py
@@ -34,6 +34,21 @@ def make_post_request():
     return inner
 
 
+@pytest.fixture
+def make_post_request_form():
+    def inner(
+        method: str, data: dict = None, headers: dict = None, json=None
+    ) -> Response:
+        data = data or {}
+        headers = headers or {}
+        json = json or {}
+        url = settings.base_api + method
+        response = post(url, json=json, headers=headers)
+        return response
+
+    return inner
+
+
 @pytest.fixture(scope='session')
 def redis_client():
     return redis.Redis(
--- tests/functional/src/test_auth_service.py
+++ tests/functional/src/test_auth_service.py
@@ -11,6 +11,11 @@ class BaseData:
     name = 'test'
     email = 'test3@me.com'
     password = 'password3'
+    headers = {
+        'Content-type': 'application/json',
+        'Accept': 'text/plain',
+        'Content-Encoding': 'utf-8',
+    }
     signup_data = {
         'name': name,
         'email': email,
@@ -41,6 +46,36 @@ def test_sign_up(make_post_request):
 def test_login(data, status, make_post_request):
     response = make_post_request('/auth/login', data)
     assert response.status_code == status
+
+
+def test_get_history(make_get_request):
+    response = make_get_request(
+        '/auth/history', params=BaseData.login_data, headers=BaseData.headers
+    )
+    assert response.status_code == HTTPStatus.OK
+
+
+def test_get_index(make_get_request):
+    response = make_get_request(
+        '', params=BaseData.login_data, headers=BaseData.headers
+    )
+    assert response.status_code == HTTPStatus.OK
+
+
+def test_get_user_profile(make_get_request):
+    response = make_get_request(
+        '/auth/profile', params=BaseData.login_data, headers=BaseData.headers
+    )
+    assert response.status_code == HTTPStatus.OK
+
+
+def test_get_refresh_token(make_get_request):
+    response = make_get_request(
+        '/auth/refresh', params=BaseData.login_data, headers=BaseData.headers
+    )
+    assert response.status_code == HTTPStatus.OK
+    data = 'http://127.0.0.1:5000/auth/login?next=%2Fauth%2Frefresh'
+    assert response.url == data
 
 
 def test_logout(make_post_request):
